4df3af508e57e4f337310ad883342b928c373f6f
Mr-Carrot888 <marip13024@gmail.com>
2025-10-19 22:13:16 +0700

    pu score

--- static/index.js
+++ static/index.js
@@ -37,7 +37,7 @@ document.addEventListener("DOMContentLoaded", function() {
 	// ===== สร้าง socket object =====
 	// ยังไม่เชื่อมต่อกับ server จนกว่าจะกด connect
 
-	const socket = io("https://d91a432137b9.ngrok-free.app", { autoConnect: false, transports: ["websocket"] });
+	const socket = io("https://9c9da0cda61b.ngrok-free.app", { autoConnect: false, transports: ["websocket"] });
 
 function drawCharacter(ctxGame, x, y, angle = 0, color = 'red', SCALE_FACTOR = 0.5) { 
     ctxGame.save();
